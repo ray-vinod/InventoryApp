5e443d835bd8380c74d307a2e5d1f7a905c0f548
vinod ray <ray_vinod@live.com>
2021-04-07 14:26:10 +0545

    feature: #4 edit identity

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,46 @@
+﻿using InventoryApp.Models.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() : base()
+        {
+        }
+
+        [Required]
+        [PersonalData]
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+
+        [PersonalData]
+        [Display(Name = "Middle Name")]
+        public string MName { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Display(Name = "Last Name")]
+        public string LName { get; set; }
+
+        [PersonalData]
+        public string City { get; set; }
+        [PersonalData]
+        public int CountryId { get; set; }
+        [PersonalData]
+        public int ProvinceId { get; set; }
+        [PersonalData]
+        public Gender Gender { get; set; }
+        [PersonalData]
+        public DateTime CreatedDate { get; internal set; } = DateTime.Now;
+        [PersonalData]
+        public string ImagePath { get; internal set; }
+
+        //Navigation Properties
+        public Country Country { get; set; }
+
+
+    }
+}
---
+++ Services/UserStateService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InventoryApp.Services
+{
+    public class UserStateService
+    {
+        public event Action OnRefreshRequeste;
+
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public string Email { get; set; }
+
+
+        public void CallPageRefresh()
+        {
+            OnRefreshRequeste?.Invoke();
+        }
+    }
+}
