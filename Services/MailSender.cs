ce671c755e7b168078de91243ec059d5481a5da0
vinod ray <ray_vinod@live.com>
2021-04-07 14:48:04 +0545

    feature: #6 add email sender

---
+++ Services/MailSender.cs
@@ -0,0 +1,39 @@
+﻿using InventoryApp.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace InventoryApp.Services
+{
+    public class MailSender : IEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+
+        public MailSender(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string emailAddress, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailAddress));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = htmlMessage;
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+            await Task.Run(async () => await smtp.SendAsync(email));
+            smtp.Disconnect(true);
+        }
+    }
+}
